e57a6eb0eaad89e0dc96ddb5bf14dfabd00e2a21
CkBc <89769471+CkBcDD@users.noreply.github.com>
2024-07-17 15:24:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '15177f58-f197-468c-8e43-f0623fde0784';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
