99be23fd72580532761bfc616a0fb42e1a8d43fe
CkBc <89769471+CkBcDD@users.noreply.github.com>
2024-07-17 15:16:48 +0800

    Update _worker.js
    customed uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '7e61af70-2e33-4a22-8e3f-d95e30bf521b';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
